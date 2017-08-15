30fbf756f6298de1c2e0e26061d9c2ddb8eb78af
Hanxi Liu <hanxi.liu@easystack.cn>
2017-08-16 01:23:40 +0800

    Add Binomial coefficient template

---
+++ Mathmatics/Binomial_coefficient/1.cpp
@@ -0,0 +1,26 @@
+//time O(n), Space O(n)
+
+#define MOD 1000000007
+#define SIZE 100005
+ 
+using namespace std;
+typedef long long int ll;
+ 
+ll inv[SIZE],fac[SIZE],finv[SIZE];
+void make()
+{
+	fac[0]=fac[1]=1;
+	finv[0]=finv[1]=1;
+	inv[1]=1;
+	for(int i=2;i<SIZE;i++)
+	{
+		inv[i]=MOD-inv[MOD%i]*(MOD/i)%MOD;
+		fac[i]=fac[i-1]*(ll) i%MOD;
+		finv[i]=finv[i-1]*inv[i]%MOD;
+	}
+}
+ll C(int a,int b)
+{
+	if(a<b) return 0;
+	return fac[a]*(finv[b]*finv[a-b]%MOD)%MOD;
+}
